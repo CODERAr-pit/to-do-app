fca9f8c4a371dc989d93d157e55fb726a7092b30
Arpit <jhaarpit1111@gmail.com>
2025-07-04 20:56:05 +0530

    All features of Delete,edit,save to local storage,checkbox,linethrough has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,95 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 import Navbar from'./components/Navbar.jsx'
 function App() {
   const[Todo,setTodo]=useState("");
   const[Todos,setTodos]=useState([]);
 
-  const handleAdd=(e)=>{
-    if (Todo=== "") return;
-    setTodos([...Todos,Todo]);
-    setTodo("");
-    
+  useEffect(()=>{
+    let Todos=JSON.parse(localStorage.getItem("Todos"))||[];
+    setTodos(Todos)
+  },[])
+
+   const saveToLs= (Todos) => {
+    localStorage.setItem("Todos",JSON.stringify(Todos))
   }
+  
+
+  const handleEdit=(index)=>{
+  const newText=prompt("Enter Your text",Todos[index].text)
+  if(newText&&newText.trim()!==""){
+    const update=Todos.map((Todo,i)=>
+      i===index ?{...Todo, text:newText}: Todo);
+     setTodos(update);}
+     saveToLs(update);}
+  
+
+  const handleDelete=(index)=>{
+    const update=Todos.filter((Todo,i)=>i!=index);
+    setTodos(update);
+    saveToLs(update);
+  }
+
+
+  const handleAdd=(e)=>{
+    if (Todo.trim()=== "") return;
+    const update=[...Todos,{text:Todo,completed:false}];
+    setTodos(update);
+    saveToLs(update);
+    setTodo("");
+  }
+ 
   return (
     <>
         <Navbar />
-        <div className=' mx-auto my-auto bg-slate-200 w-3/4 min-h-screen'>
+        <div className=' mx-auto my-5 rounded-2xl rounded-b-none bg-slate-300 w-1/3 min-h-screen'>
         <div>
           <div className='mx-3 font-semibold text-lg'>Add a Task:</div>
+          
           <input
            value={Todo}
            onChange={(e)=>{
             setTodo(e.target.value);
-          }} type='text'placeholder='Add new task' className='w-4/5 bg-slate-500 rounded-3xl mx-3 text-center'/>
+          }} type='text'placeholder='Add new task' className='w-4/5 text-black placeholder-slate-500 rounded-3xl mx-3 text-center'/>
           <button 
           value={Todo}
           onClick={handleAdd} 
-          className='rounded-md  bg-orange-500  p-1'>Add</button>
+          className='gap-22 rounded-md  bg-orange-500  p-1'>Add</button>
         </div>
         <div>
           <div className='mx-3 font-semibold text-lg my-7'>Your To-Do:</div>
         </div>
         <div>
-          <ol>{Todos.map((Todo,index)=>(
-            <li key={index}>{Todo}</li>
+          <ol >{
+          Todos.map((Todo,index)=>(
+            <li key={index} className='my-4'>
+              <input type='checkbox'className='mx-4' checked={Todo.completed}
+              onChange={() => {
+              const updated = Todos.map((item, i) =>
+              i === index ? { ...item, completed: !item.completed } : item
+    );
+    setTodos(updated);
+    saveToLs(update);
+  }}/>        
+              <span className={Todo.completed ?'line-through':''}>
+              {Todo.text}
+              </span>
+            <div className='mx-4'>
+              <button 
+              onClick={()=>handleEdit(index)} 
+              className='rounded-md p-1 bg-green-400 gap-22 mx-2'>Edit</button>
+              <button 
+              onClick={()=>handleDelete(index)} 
+              className='rounded-md  bg-red-500  p-1'>Delete</button>
+            </div>
+            </li>
           ))
           }</ol>
             
         </div>
         </div>
     </>
-  )
-}
+  )}
+
 
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-   <nav className='bg-blue-900 text-red-500 w-full h-8 grid items-center ' >
+   <nav className='bg-blue-800 text-white w-full h-12 grid items-center rounded-3xl rounded-b-none ' >
     <div className='flex justify-between'>
         <div className='bold  mx-4'>
-        i-Todo
+        Xe-Todo
          </div>
     <div className='flex gap-4 mx-4'>
        <div>Home</div>
